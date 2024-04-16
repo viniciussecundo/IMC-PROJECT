31b4e5f0c5ba74586d7d1714f13ed3bd771726f1
viniciussecundo <viniciudxd1@gmail.com>
2024-04-16 02:55:29 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const button = document.getElementById("button")
+const nome = document.getElementById("nome")
+const peso = document.getElementById("peso")
+const altura = document.getElementById("altura")
+const resultado = document.getElementById("resultado")
+
+const getText = (imc) => {
+  if (imc > 40) return "Obesidade grau III"
+  if (imc > 35) return "Obesidade grau II"
+  if (imc > 30) return "Obesidade grau I"
+  if (imc > 25) return "levemente acima do peso"
+  if (imc > 18.5) return "Peso ideal"
+  return "Abaixo do peso"
+}
+
+const imcCalc = () => {
+  if (!peso.value || !altura.value || !nome.value)
+    return (resultado.textContent = " Ainda restam campos a serem preenchidos")
+
+  const valorImc = +peso.value / (+altura.value * +altura.value).toFixed(1)
+  resultado.textContent = `${nome.value} - ${getText(valorImc)}`
+}
+
+button.addEventListener("click", imcCalc)
